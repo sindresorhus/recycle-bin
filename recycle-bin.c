69a841a0612b721684c64cf4d789eafb8daf7a2b
medusalix <ss7@live.de>
2019-02-05 18:54:03 +0100

    Remove old API

--- recycle-bin.c
+++ recycle-bin.c
@@ -15,84 +15,6 @@
 	return result;\
 }
 
-int delete_files(int count, wchar_t **files) {
-	size_t len = count;
-
-	for (int i = 1; i < count; i++) {
-		len += wcslen(files[i]);
-	}
-
-	wchar_t *from = malloc(len * sizeof(wchar_t));
-
-	int pos = 0;
-
-	for (int i = 1; i < count; i++) {
-		wcscpy(&from[pos], files[i]);
-		pos += wcslen(files[i]) + 1;
-	}
-
-	from[pos] = '\0';
-
-	assert(++pos == len && "position/length mismatch");
-
-	SHFILEOPSTRUCTW op;
-
-	op.hwnd = NULL;
-	op.wFunc = FO_DELETE;
-	op.pFrom = from;
-	op.pTo = NULL;
-	op.fFlags = FOF_ALLOWUNDO | FOF_NO_UI;
-
-	int ret = SHFileOperationW(&op);
-
-	free(from);
-
-	return ret;
-}
-
-int delete_files_new(int count, wchar_t **files) {
-	CHECK(CoInitializeEx(NULL, COINIT_MULTITHREADED));
-
-	IFileOperation *op;
-
-	CHECK(CoCreateInstance(
-		&CLSID_FileOperation,
-		NULL,
-		CLSCTX_ALL,
-		&IID_IFileOperation,
-		(void**)&op
-	));
-
-	CHECK_OBJ(op, op->lpVtbl->SetOperationFlags(
-		op,
-		FOFX_ADDUNDORECORD |
-		FOFX_RECYCLEONDELETE |
-		FOF_NOERRORUI |
-		FOF_SILENT |
-		FOFX_EARLYFAILURE
-	));
-
-	PCIDLIST_ABSOLUTE list[count - 1];
-
-	for (int i = 1; i < count; i++) {
-		for (wchar_t *chr = files[i]; *chr; chr++) {
-			if (*chr == L'/') {
-				*chr = L'\\';
-			}
-		}
-
-		list[i - 1] = ILCreateFromPath(files[i]);
-	}
-
-	IShellItemArray *items;
-
-	CHECK_OBJ(op, SHCreateShellItemArrayFromIDLists(count - 1, list, &items));
-	CHECK_OBJ(op, op->lpVtbl->DeleteItems(op, (IUnknown*)items));
-	CHECK_OBJ(op, op->lpVtbl->PerformOperations(op));
-
-	return op->lpVtbl->Release(op);
-}
-
 int wmain(int argc, wchar_t **argv) {
 	if (argc == 2) {
 		if (wcscmp(argv[1], L"--version") == 0) {
@@ -109,9 +31,60 @@ int wmain(int argc, wchar_t **argv) {
 		return 1;
 	}
 
+	int count = argc - 1;
+	wchar_t **files = argv + 1;
+
+	CHECK(CoInitializeEx(NULL, COINIT_MULTITHREADED));
+
+	IFileOperation *op;
+
+	CHECK(CoCreateInstance(
+		&CLSID_FileOperation,
+		NULL,
+		CLSCTX_ALL,
+		&IID_IFileOperation,
+		(void**)&op
+	));
+
 	if (IsWindows8OrGreater()) {
-		return delete_files_new(argc, argv);
+		CHECK_OBJ(op, op->lpVtbl->SetOperationFlags(
+			op,
+			FOFX_ADDUNDORECORD |
+			FOFX_RECYCLEONDELETE |
+			FOF_NOERRORUI |
+			FOF_SILENT |
+			FOFX_EARLYFAILURE
+		));
+	} else {
+		CHECK_OBJ(op, op->lpVtbl->SetOperationFlags(
+			op,
+			FOF_NO_UI |
+			FOF_ALLOWUNDO |
+			FOF_NOERRORUI |
+			FOF_SILENT |
+			FOFX_EARLYFAILURE
+		));
 	}
 
-	return delete_files(argc, argv);
+	PCIDLIST_ABSOLUTE list[count];
+
+	for (int i = 0; i < count; i++) {
+		for (wchar_t *chr = files[i]; *chr; chr++) {
+			if (*chr == L'/') {
+				*chr = L'\\';
+			}
+		}
+
+		IShellItem *item;
+
+		list[i] = ILCreateFromPath(files[i]);
+	}
+
+	IShellItemArray *items;
+
+	CHECK_OBJ(op, SHCreateShellItemArrayFromIDLists(count, list, &items));
+	CHECK_OBJ(op, op->lpVtbl->DeleteItems(op, (IUnknown*)items));
+	CHECK_OBJ(op, op->lpVtbl->PerformOperations(op));
+
+	return op->lpVtbl->Release(op);
 }
